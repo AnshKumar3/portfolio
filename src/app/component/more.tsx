a9036343627231fbae1c2869621ca5129c571b26
Ansh Kumar <88948265+AnshKumar3@users.noreply.github.com>
2024-08-23 19:43:02 +0530

    first commit

---
+++ src/app/component/more.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import React from 'react'
+import "../styles.css";
+import "../type.css"
+function More() {
+  return (
+    <div className="grid h-screen place-items-center responsive-div">
+<div className="input-wrapper">
+<input aria-label="Ask us anything"></input>
+<span className="placeholder"></span>
+</div>
+<div className="typewriter">
+<h1>Creative Fullstack Web Developer</h1>
+</div>
+<div className="gagan">
+<h1>G a g a n</h1>
+</div>
+<h1 className="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Gagan</h1>
+
+<img
+src="https://i.imgur.com/MK3eW3Am.jpg"
+alt="Katherine Johnson"
+/>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default More
+
+
+
+
+
+
+
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import "./styles.css";
 import "./type.css"; import styles from './Navbar.module.css';
-import App from "./component/particles";
+
 
 import ScrollReveal from 'scrollreveal';
 
@@ -13,44 +13,21 @@ import type { Container, Engine } from "tsparticles-engine";
 
 import { loadSlim } from "tsparticles-slim";
 import Link from "next/link";
-import About from "./component/about";
 import Navbar from "./component/nav";
+import dynamic from "next/dynamic";
+import More from "./component/more";
+const App = dynamic(() => import("./component/particles"),{ssr:false})
+  ;
+const About = dynamic(() => import("./component/about"),{ssr:false})
 
 export default function Home() {
 
   return (
     <>
-
-      <div className="grid h-screen place-items-center responsive-div">
-
         <App />
-
-
         <Navbar />
-        <div className="input-wrapper">
-          <input aria-label="Ask us anything"></input>
-          <span className="placeholder"></span>
-        </div>
-        <div className="typewriter">
-          <h1>Creative Fullstack Web Developer</h1>
-        </div>
-        <div className="gagan">
-          <h1>G a g a n</h1>
-        </div>
-        <h1 className="mt-1 text-lg font-semibold text-white sm:text-slate-900 md:text-2xl dark:sm:text-white">Gagan</h1>
-
-        <img
-          src="https://i.imgur.com/MK3eW3Am.jpg"
-          alt="Katherine Johnson"
-        />
-
-
-
-      </div>
-
+        <More></More>
       <About />
-      <div style={{ height: "2000px" }}>asd</div>
-
     </>
   );
 }
